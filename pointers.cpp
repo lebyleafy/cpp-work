380766de9327c15acdfb3769e61f001f57e6ce75
lea <73567807+lebyleafy@users.noreply.github.com>
2021-10-30 08:07:48 +0700

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,33 @@
+void Unknown(int *p, int num);
+void HardToFollow(int *p, int q, int *num);
+
+void Unknown(int *p, int num) {
+  int *q;
+
+  q = &num;
+  *p = *q + 2;
+  num = 7;
+}
+
+void HardToFollow(int *p, int q, int *num) {
+  *p = q + *num;
+  *num = q;
+  num = p;
+  p = &q;
+  Unknown(num, *p);
+}
+
+main() {
+  int *q;
+  int trouble[3];
+
+  trouble[0] = 1;
+  q = &trouble[1];
+  *q = 2;
+  trouble[2] = 3;
+
+  HardToFollow(q, trouble[0], &trouble[2]);
+  Unknown(&trouble[0], *q);
+
+  cout << *q << " " << trouble[0] << " " << trouble[2];
+}
